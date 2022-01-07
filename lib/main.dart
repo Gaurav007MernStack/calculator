2efad2a3f4ff1de21c018e8a405baf8071e26666
FlutLab.io <>
2022-01-07 08:50:42 +0000

    final

--- lib/main.dart
+++ lib/main.dart
@@ -79,7 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('CalculatorR'),
+        title: Text('CalculatorRstatrt'),
       ),
       body: Container(
         child: Column(
